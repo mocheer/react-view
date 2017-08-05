5c18a5242c893d46decc4ec234a6919c883333eb
mocheer <mocheer@foxmail.com>
2017-08-05 15:28:01 +0800

    update

--- src/modules/Widgets.js
+++
@@ -1,5 +0,0 @@
-exports.ContextMenu = require('./ContextMenu')
-exports.LegendBox = require('./map/LegendBox')
-exports.LegendBox2 = require('./map/LegendBox2')
-exports.ToolTip = require('./ToolTipEx')
-
--- webpack.config.js
+++ webpack.config.js
@@ -50,24 +50,11 @@ switch (env.task) {
 }
 module.exports = {
     entry: {
-        'eventcontrol': './src/modules/EventControl',
-        // 'water/chart': './src/modules/water/WaterChart',
-        "topicbar": "./src/modules/Topicbar.js",
-        "typhoon/typhoon": "./src/modules/typhoon/Typhoon.js",
-        // "typhoon/typhooninfobox": "./src/modules/typhoon/TyphoonInfoBox.js",
-        // "typhoon/typhoonplayer": "./src/modules/typhoon/TyphoonPlayer.js",
-        "extra/areacontrol": "./src/modules/map/AreaControl.js",
-        "extra/mapnav": "./src/modules/MapNav.js",
-        "extra/controlbox": "./src/modules/map/ControlBox.js",
-        "extra/widgets": "./src/modules/Widgets.js",
-        'extra/pancontrol': './src/modules/map/PanControl.js',
-        'extra/zoomcontrol': './src/modules/map/ZoomControl.js'
-
+        'react-view': './index.js'
     },
     output: {
         path: dir,
-        filename: '../tree-example/tree/modules/[name].js',
-        libraryTarget: "amd"
+        filename: 'dist/[name].js'
     },
     externals: {
         'react': 'react',
